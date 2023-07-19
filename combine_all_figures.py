bb90b8e57109da071f6d805fb44d1e4eff7a2ecc
Javed Ali <javedali28@gmail.com>
2023-07-19 16:09:47 -0500

    Combine all figures into one for all storms discharge

---
+++ combine_all_figures.py
@@ -0,0 +1,39 @@
+# Description: Combine all storm event figures into one figure
+# Author: Javed Ali (javed.ali@ucf.edu)
+# Date: July 19, 2023
+
+# Import libraries
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# List of image paths
+images = [
+    "figures/Hurricane Arthur_discharge.png",
+    "figures/Hurricane Dorian_discharge.png",
+    "figures/Hurricane Hanna_discharge.png",
+    "figures/Hurricane Irene_discharge.png",
+    "figures/Hurricane Sandy_discharge.png",
+    "figures/Tropical Storm Barry_discharge.png",
+    "figures/Tropical Storm Jose_discharge.png",
+    "figures/Tropical Storm Ogla_discharge.png",
+    "figures/Tropical Storm Philippe_discharge.png",
+]
+
+# Open images and resize if needed
+imgs = [Image.open(i) for i in images]
+
+# Assume all images have the same size as the first one
+width, height = imgs[0].size
+
+# Create an empty image with appropriate size to place the smaller images
+final_img = Image.new("RGB", (3 * width, 3 * height))
+
+# Iterate over the smaller images and paste them into the final image
+for i, img in enumerate(imgs):
+    final_img.paste(img, ((i % 3) * width, (i // 3) * height))
+
+# Save the final image
+final_img.save("all_storm_events_discharge_final.png", dpi=(300, 300))
+
+# Show the final image
+plt.imshow(final_img)
