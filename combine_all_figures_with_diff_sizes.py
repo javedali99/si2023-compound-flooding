8bd84e7c0a21d19295d3303b80ba0558764ec6fd
Javed Ali <javedali28@gmail.com>
2023-07-20 12:28:02 -0500

    Remove commented code in combine_diff_size_imgs

--- combine_all_figures_with_diff_sizes.py
+++ combine_all_figures_with_diff_sizes.py
@@ -21,34 +21,6 @@ images = [
     "max_gauge_8518750_storm_surge_everythingin1.png",
 ]
 
-# # Open images and get their sizes
-# imgs = [Image.open(i) for i in images]
-# sizes = [img.size for img in imgs]
-
-# # Get maximum width and height
-# max_width = max([size[0] for size in sizes])
-# max_height = max([size[1] for size in sizes])
-
-# # Resize all images to the same size
-# imgs = [img.resize((max_width, max_height)) for img in imgs]
-
-# # Create an empty image with appropriate size to place the smaller images
-# # The color of the new image is set to white
-# final_img = Image.new("RGB", (3 * max_width, 2 * max_height), "white")
-
-# # Iterate over the smaller images and paste them into the final image
-# for i, img in enumerate(imgs):
-#     final_img.paste(img, ((i % 3) * max_width, (i // 3) * max_height))
-
-
-# # Save the final image with a high DPI for better quality
-# final_img.save("all_stations_irene_geoclaw.png", dpi=(2000, 2000))
-
-# Show the final image
-# plt.imshow(final_img)
-# plt.axis("off")  # to hide axis
-# plt.show()
-
 # Open images and get their sizes
 imgs = [Image.open(i) for i in images]
 sizes = [img.size for img in imgs]
